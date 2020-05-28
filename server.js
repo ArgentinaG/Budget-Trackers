0d6261ea4287b6a2fe3ef88a8c159c24a12fde23
ArgentinaG <59855898+ArgentinaG@users.noreply.github.com>
2020-05-28 16:21:01 -0500

    updated service-worker.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// mongoose.connect(MONGODB_URI);
 
 mongoose.connect("mongodb://localhost/budget", {
     useNewUrlParser: true,
